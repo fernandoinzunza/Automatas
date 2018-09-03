6a1401e4cbb158671e9b51648185f6fcfb275088
eljon <jon_leester@hotmail.com>
2018-09-02 23:54:33 -0600

    save boton

--- EscanerLSQL/Form1.cs
+++ EscanerLSQL/Form1.cs
@@ -21,5 +21,10 @@ namespace EscanerLSQL
         {
             MessageBox.Show("Hola mundo");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo 2");
+        }
     }
 }
